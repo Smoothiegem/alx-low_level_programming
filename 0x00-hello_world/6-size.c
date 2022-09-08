18aa35b90da697b5be1f2e6c7dba1e1b67502c2b
smoothiegem <peaceamavu00@gmail.com>
2022-09-08 15:46:10 -0700

    father

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -7,10 +8,9 @@ int main(void)
 {
 	int a;
 	long int b;
-	long int b;
+	long long int c;
 	char d;
 	float f;
-
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
@@ -18,4 +18,3 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
-
